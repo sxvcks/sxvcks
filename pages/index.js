3eeac7231c88cfc89592dc51a7b1ff82765600e5
sxvcks <97019335+sxvcks@users.noreply.github.com>
2022-01-03 09:39:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Witaj na mojej stronie tiktok.com/@sxvcks!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
